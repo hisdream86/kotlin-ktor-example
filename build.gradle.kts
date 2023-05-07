784511de1794f7cefddedb4e8f1ddb08f9fdef82
Kevin Jang <kevin.jang@sendbird.com>
2023-05-07 16:17:17 +0900

    feat: initialize a project

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+    id("io.ktor.plugin") version "2.3.0"
+}
+
+group = "com.example"
+version = "0.0.1"
+application {
+    mainClass.set("com.example.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+}
