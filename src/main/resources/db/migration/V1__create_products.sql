fc73a31b94e975f0e8829efeefbb8cfb6c8cca8d
hisdream86 <hisdream86@gmail.com>
2023-10-09 10:16:44 +0900

    feat: Update PK

--- src/main/resources/db/migration/V1__create_products.sql
+++ src/main/resources/db/migration/V1__create_products.sql
@@ -1,10 +1,12 @@
 CREATE TABLE products (
+    id          BIGSERIAL       NOT NULL,
     product_id  UUID            NOT NULL,
     name        VARCHAR(128)    NOT NULL,
     description TEXT            NULL,
     price       NUMERIC         NULL,
 
-    PRIMARY KEY (product_id)
+    PRIMARY KEY (id)
 );
 
+CREATE INDEX product_id ON products (name);
 CREATE INDEX idx_name ON products (name);
